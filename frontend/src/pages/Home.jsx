6545435e0730e33756f0ac43afc8d18fdff83a71
LD.dev <ld.devarg@gmail.com>
2026-02-03 18:33:03 -0300

    Correccion imagenes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,11 @@ import Button from "../components/Button";
 
 export default function Home() {
     const [activeForm, setActiveForm] = useState("viajes");
+    const logoSrc = `${import.meta.env.BASE_URL}logo_transvaal.png`;
 
     return (
         <div className='flex h-full w-full flex-col items-center justify-start gap-6 pt-10 bg-gray-100 dark:bg-gray-900'>
-            <img src="/logo_transvaal.png" className='h-30 w-30 rounded-full' alt="Logo" />
+            <img src={logoSrc} className='h-30 w-30 rounded-full' alt="Logo" />
             <div className="flex w-full gap-4 justify-center flex-row">
                 <Button
                     onClick={() => setActiveForm("viajes")}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import LoginForm from '../components/LoginForm.jsx'
 
 export default function Login() {
+    const logoSrc = `${import.meta.env.BASE_URL}logo_transvaal.png`
+
     return (
-        <div className='flex h-full vw-100 flex-col items-center justify-start gap-6 pt-10 bg-gray-100 dark:bg-gray-900'>
-            <img src="/logo_transvaal.png" className='h-30 w-30 rounded-full' alt="Logo" />
+        <div className='flex h-full w-full flex-col items-center justify-start gap-6 pt-10 bg-gray-100 dark:bg-gray-900'>
+            <img src={logoSrc} className='h-30 w-30 rounded-full' alt="Logo" />
             <h2 className='dark-black text-xl dark:text-white'>Inicia Sesion</h2>
             <LoginForm />
         </div>
